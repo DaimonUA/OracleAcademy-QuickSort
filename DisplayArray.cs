18aaaa0a73f35e921b58c24e60c64472dd733070
DaimonUA <DaimonUA@gmail.com>
2017-06-05 00:12:53 +0300

    Add project files.

---
+++ DisplayArray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace QuickSort
+{
+    class DisplayArray
+    {
+        /// <summary>
+        /// выводит текущее состояние массива:
+        /// array - выводимый массив (передается по ссылке);
+        /// step - шаг операции сортировкиж;
+        /// basicItem - индекс текущего опорного элемента сортировки массива (необязательный)
+        /// </summary>
+        /// <param name="currArray"></param>
+        /// <param name="step"></param>
+        /// <param name="basicItem"></param>
+        internal static void ListCurrentlyArray(ref int[] currArray, int step, int basicItem = -1)
+        {
+            Console.Write("step {0}:\t", step);
+            char addSymb = '\0';
+
+            var bgcBasic = Console.BackgroundColor;
+            for (int i = 0; i < currArray.Length; i++)
+            {
+                Console.Write("{0}", addSymb);
+                if (i == basicItem)
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                }
+                Console.Write("{0}", currArray[i]);
+                Console.BackgroundColor = bgcBasic;
+                addSymb = ',';
+            }
+            Console.BackgroundColor = bgcBasic;
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ QuickSort.cs
@@ -0,0 +1,96 @@
+﻿using System;
+namespace QuickSort
+{
+    class QuickSort
+    {
+        static void Main()
+        {
+            int[] myArray = GetArray();
+            int step = 0; //этап сортировки
+            DisplayArray.ListCurrentlyArray(ref myArray,step);
+            OrderArray(ref myArray,step,0,myArray.Length-1);
+            Console.ReadKey();
+        }
+
+        static void OrderArray(ref int[] array, int step, int leftIndex, int rightIndex)
+        {
+            if (leftIndex > rightIndex) return;
+            step++;
+            int begin = leftIndex;
+            int end = rightIndex;
+            int pivotIndex = 0;
+            bool rebuilding = false;
+            while (!GetPivot(ref array, ref begin, ref end))
+            {
+                pivotIndex = end;
+                DisplayArray.ListCurrentlyArray(ref array, step, pivotIndex);
+                rebuilding = true;
+            }
+            if (rebuilding)
+            {
+                OrderArray(ref array, step, leftIndex, pivotIndex-1);
+                OrderArray(ref array, step, pivotIndex+1, rightIndex);
+            }
+        }
+
+        /// <summary>
+        /// возвращает false, если опорный элемент выделенного отрезка массива (с indexInc по indexDec) находится не на своем месте;
+        /// при это происходит перестроение элентов массива.
+        /// Иначе возвращется true.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="indexInc"></param>
+        /// <param name="indexDec"></param>
+        /// <returns></returns>
+        static bool GetPivot(ref int[] array, ref int indexInc, ref int indexDec)
+        {
+            int i = indexInc;
+            int j = indexDec;
+            int pivot = array[j];
+            while ((pivot > array[i]) && (i<j))
+                {
+                i++;
+                }
+
+            if (i < j)
+            {
+                RebuildItems(ref array, i, j);
+                indexDec--;
+                indexInc = i;
+                return false;
+
+            }
+            
+            return indexInc >= (--indexDec);
+
+        }
+
+        static void RebuildItems(ref int[] array, int leftIndex, int rightIndex)
+        {
+               if (rightIndex-leftIndex > 1)
+                {
+                    int tmp = array[rightIndex - 1];
+                    array[rightIndex-1] = array[rightIndex];
+                    array[rightIndex] = array[leftIndex];
+                    array[leftIndex] = tmp;
+                 }
+                 else
+                {
+                    array[rightIndex] = array[leftIndex]+array[rightIndex];
+                    array[leftIndex] = array[rightIndex]-array[leftIndex];
+                    array[rightIndex] = array[rightIndex]-array[leftIndex];
+                }
+
+        }
+
+        static int[] GetArray()
+        {
+            int[] resArray = { 3, 7, 8, 5, 2, 1, 9, 5, 4 }; //from Wiki
+            //int[] resArray = { 3, 7, 8, 5, 2, 1, 9, 6, 14 };
+            //int[] resArray = { -34, 45, 28, 10, 4, 47, 23, 15, 61, 88, 55 };
+            //int[] resArray = { -34,0,1,2,71,70, -34 };
+            return resArray;
+        }
+
+    }
+}
